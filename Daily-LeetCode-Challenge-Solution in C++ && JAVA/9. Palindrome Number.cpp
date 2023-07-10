e1d93fd377a89aba59919aa6ba8a300d152018c5
Aditya <97380595+ADITYAGABA1322@users.noreply.github.com>
2023-07-10 15:09:35 +0530

    Update 9. Palindrome Number.cpp

--- Daily-LeetCode-Challenge-Solution in C++ && JAVA/9. Palindrome Number.cpp
+++ Daily-LeetCode-Challenge-Solution in C++ && JAVA/9. Palindrome Number.cpp
@@ -42,3 +42,25 @@ public:
 
 
 Java:  
+
+
+// Time Complexity : O(logn) where n is the number of digits in the number and space complexity is O(1)
+
+
+
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){  // if the number is negative
+            return false;  // return false
+        }
+        int temp = x;  // variable to store the number
+        long reverse = 0;  // variable to store the reverse of the number
+        while(temp != 0){  // iterate until the number becomes 0
+            reverse = reverse * 10 + temp % 10;  // update the reverse
+            temp /= 10;  // update the number
+        }
+        return reverse == x;  // return true if the number is palindrome
+    }
+}
