cdcda1ce785ca35f5ad199cdbbb9602e8f8db700
Aditya <97380595+ADITYAGABA1322@users.noreply.github.com>
2023-08-14 22:43:53 +0530

    Update 2369. Check if There is a Valid Partition For The Array.dart

--- Daily-LeetCode-Challenge-Solution in C++ && JAVA/2369. Check if There is a Valid Partition For The Array.dart
+++ Daily-LeetCode-Challenge-Solution in C++ && JAVA/2369. Check if There is a Valid Partition For The Array.dart
@@ -146,6 +146,24 @@ public class Solution {
 
 JavaScript:
 
+                                        
+// Time Complexity : O(n) where n is the size of the vector nums and space complexity is O(1)
+
+var validPartition = function(nums) {
+    let n = nums.length; // variable to store the size of the vector nums
+    let dp = new Array(3).fill(false);  // vector to store the result
+    dp[0] = true;
+    for(let i=0; i<n; i++){
+        let ans = false; // variable to store the ans
+        let index = i+1; // variable to store the dpindex
+        if(i>0 && nums[i] == nums[i-1]) ans |= dp[(index-2)%3]; // if the current element is equal to the previous element
+        if(i>1 && nums[i] == nums[i-1] && nums[i] == nums[i-2]) ans |= dp[(index-3)%3]; // if the current element is equal to the previous two elements
+        if(i>1 && nums[i] == nums[i-1]+1 && nums[i] == nums[i-2]+2) ans |= dp[(index-3)%3]; // if the current element is equal to the previous two elements
+        dp[index%3] = ans; // update the dp
+    }
+    return dp[n%3]; // return the dp[n%3]
+};
+
 
 
 Swift:
