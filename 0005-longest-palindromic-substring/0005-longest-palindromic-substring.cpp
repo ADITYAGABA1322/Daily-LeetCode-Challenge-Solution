410bee90f8400e303efc9aac2a806490945c87e8
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-01-25 23:09:39 +0530

    Time: 6 ms (97.34%), Space: 8.2 MB (62.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string modify = "#";
-        for(char c : s){
-            modify += c;
-            modify += '#';
-        }
-        int n = modify.size();
-        vector<int> p(n , 0);
-        int center = 0 , maxLen = 0 , maxCenter = 0 , right = 0;
+        int n = s.size();
+        if(s.empty()) return "";
+        int start = 0 , len = 0;
         for(int i=0; i<n; i++){
-            int mirror = 2*center - i;
-            if(i<right) p[i] = min(right - i , p[mirror]);
-            int a = i+(1+p[i]);
-            int b = i-(1+p[i]);
-            while(a<n && b>=0 && modify[a] == modify[b]){
-                p[i]++;
-                a++;
-                b--;
+            int l  = i , r = i;
+            while(r <n-1 && s[r] == s[r+1]) r++;
+            i = r;
+            while(l>0 && r<n-1 && s[l-1] == s[r+1]){
+                l--;
+                r++;
             }
-            if(i+p[i] > right){
-                center = i;
-                right = i+p[i];
-            }
-            if(p[i] > maxLen){
-                maxLen = p[i];
-                maxCenter = i;
+            if(r-l+1 > len){
+                len = r-l+1;
+                start = l;
             }
         }
-        int start = (maxCenter - maxLen)/2;
-        return s.substr(start , maxLen);
+        return s.substr(start , len);
     }
 };
