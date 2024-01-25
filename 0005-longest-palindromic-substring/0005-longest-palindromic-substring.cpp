49157ed17515364f671920cbeb057ffc08f9cf32
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-01-25 23:07:50 +0530

    Time: 7 ms (94.75%), Space: 8.2 MB (62.16%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if(s.empty()) return "";
+        int start = 0 , len = 0;
+        for(int i=0; i<n; i++){
+            int l  = i , r = i;
+            while(r <n-1 && s[r] == s[r+1]) r++;
+            i = r;
+            while(l>0 && r<n-1 && s[l-1] == s[r+1]){
+                l--;
+                r++;
+            }
+            if(r-l+1 > len){
+                len = r-l+1;
+                start = l;
+            }
+        }
+        return s.substr(start , len);
+    }
+};
