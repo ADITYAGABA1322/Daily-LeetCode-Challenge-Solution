e2d2d8652d95b159065c6a2e2b309b01f74b5a99
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-06-26 18:47:52 +0530

    Time: 9 ms (65.18%), Space: 24.1 MB (10.43%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -1,6 +1,27 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
-        
+        int count = 0;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = i + 1; j < words.size(); j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+private:
+    bool isPrefixAndSuffix(string& prefix, string& word) {
+        if (word.size() < prefix.size()) {
+            return false;
+        }
+        return word.substr(0, prefix.size()) == prefix && word.substr(word.size() - prefix.size()) == prefix;
     }
 };
