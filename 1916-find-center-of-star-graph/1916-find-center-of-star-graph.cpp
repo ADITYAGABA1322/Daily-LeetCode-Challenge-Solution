29b17dabfa336ac9d6cc5d86c154c90b0f7eccec
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-06-27 19:43:13 +0530

    Time: 131 ms (75.08%), Space: 70.6 MB (98.95%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -3,7 +3,7 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         int u1 = edges[0][0], v1 = edges[0][1];
         int u2 = edges[1][0], v2 = edges[1][1];
-        if  (u1 == u2 || u1 == v2) return u1;
-        return v1;
+        if  (v1 == v2 || v1 == u2) return v1;
+        return u1;
     }
 };
