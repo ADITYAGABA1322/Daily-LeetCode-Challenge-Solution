72ed85dd2f989959f1f987bdc3e91e4af1eaebfc
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-06-29 14:08:00 +0530

    Time: 276 ms (71.45%), Space: 119.4 MB (73.14%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -1,17 +1,52 @@
+// class Solution {
+// public:
+//     long long maximumImportance(int n, vector<vector<int>>& roads) {
+//         vector<int> freq(n, 0);
+//         for (auto road : roads){
+//             freq[road[0]]++;
+//             freq[road[1]]++;
+//         }
+//         sort(freq.begin(), freq.end());
+//         long long maxI = 0, val = 1;
+//         for (long long f : freq) {
+//             maxI +=  f*val++;
+            
+//         }
+//         return maxI;
+//     }
+// };
+
+// or using heap
+
+#include <vector>
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<int> freq(n, 0);
-        for (auto road : roads){
-            freq[road[0]]++;
-            freq[road[1]]++;
+        vector<int> cityFrequency(n, 0);
+        
+        // Count how many times each city appears in the roads list
+        for (const auto& road : roads) {
+            cityFrequency[road[0]]++;
+            cityFrequency[road[1]]++;
         }
-        sort(freq.begin(), freq.end());
-        long long maxI = 0, val = 1;
-        for (long long f : freq) {
-            maxI +=  f*val++;
-            
+        
+        // Use a priority queue to manage frequencies
+        priority_queue<int> pq;
+        for (int freq : cityFrequency) {
+            pq.push(freq);
         }
-        return maxI;
+        
+        long long totalImportance = 0;
+        int currentValue = n;
+        // Assign the highest values to the most frequently occurring cities
+        while (!pq.empty()) {
+            int freq = pq.top(); pq.pop();
+            totalImportance += static_cast<long long>(freq) * currentValue--;
+        }
+        
+        return totalImportance;
     }
 };
