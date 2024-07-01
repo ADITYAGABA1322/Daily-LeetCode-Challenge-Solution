2d8bf8139419289b2aa4a2f70696561c37d7a6ea
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-01 11:52:19 +0530

    Time: 366 ms (59.92%), Space: 176.2 MB (86.67%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -32,56 +32,103 @@
 //     }
 // };
 
+// #include <vector>
+// using namespace std;
+
+// class NumArray {
+// private:
+//     vector<int> tree;
+//     int n;
+
+//     void buildTree(vector<int>& nums, int treeIndex, int l, int r) {
+//         if (l == r) {
+//             tree[treeIndex] = nums[l];
+//             return;
+//         }
+//         int mid = l + (r - l) / 2;
+//         buildTree(nums, 2 * treeIndex + 1, l, mid);
+//         buildTree(nums, 2 * treeIndex + 2, mid + 1, r);
+//         tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+//     }
+
+//     void updateTree(int treeIndex, int l, int r, int idx, int val) {
+//         if (l == r) {
+//             tree[treeIndex] = val;
+//             return;
+//         }
+//         int mid = l + (r - l) / 2;
+//         if (idx <= mid) updateTree(2 * treeIndex + 1, l, mid, idx, val);
+//         else updateTree(2 * treeIndex + 2, mid + 1, r, idx, val);
+//         tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+//     }
+
+//     int sumRangeTree(int treeIndex, int l, int r, int i, int j) {
+//         if (i > r || j < l) return 0;
+//         if (i <= l && r <= j) return tree[treeIndex];
+//         int mid = l + (r - l) / 2;
+//         return sumRangeTree(2 * treeIndex + 1, l, mid, i, j) + sumRangeTree(2 * treeIndex + 2, mid + 1, r, i, j);
+//     }
+
+// public:
+//     NumArray(vector<int>& nums) {
+//         if (nums.empty()) return;
+//         n = nums.size();
+//         tree.resize(4 * n); // Allocate memory for segment tree
+//         buildTree(nums, 0, 0, n - 1);
+//     }
+
+//     void update(int index, int val) {
+//         updateTree(0, 0, n - 1, index, val);
+//     }
+
+//     int sumRange(int i, int j) {
+//         return sumRangeTree(0, 0, n - 1, i, j);
+//     }
+// };
 #include <vector>
 using namespace std;
 
 class NumArray {
 private:
-    vector<int> tree;
+    vector<int> fenwickTree;
+    vector<int> nums;
     int n;
 
-    void buildTree(vector<int>& nums, int treeIndex, int l, int r) {
-        if (l == r) {
-            tree[treeIndex] = nums[l];
-            return;
+    // Function to update the Fenwick Tree
+    void updateFenwickTree(int i, int delta) {
+        while (i <= n) {
+            fenwickTree[i] += delta;
+            i += i & (-i); // Move to the next index to update
         }
-        int mid = l + (r - l) / 2;
-        buildTree(nums, 2 * treeIndex + 1, l, mid);
-        buildTree(nums, 2 * treeIndex + 2, mid + 1, r);
-        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
     }
 
-    void updateTree(int treeIndex, int l, int r, int idx, int val) {
-        if (l == r) {
-            tree[treeIndex] = val;
-            return;
+    // Function to get prefix sum up to index i
+    int getPrefixSum(int i) {
+        int sum = 0;
+        while (i > 0) {
+            sum += fenwickTree[i];
+            i -= i & (-i); // Move to parent index
         }
-        int mid = l + (r - l) / 2;
-        if (idx <= mid) updateTree(2 * treeIndex + 1, l, mid, idx, val);
-        else updateTree(2 * treeIndex + 2, mid + 1, r, idx, val);
-        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
-    }
-
-    int sumRangeTree(int treeIndex, int l, int r, int i, int j) {
-        if (i > r || j < l) return 0;
-        if (i <= l && r <= j) return tree[treeIndex];
-        int mid = l + (r - l) / 2;
-        return sumRangeTree(2 * treeIndex + 1, l, mid, i, j) + sumRangeTree(2 * treeIndex + 2, mid + 1, r, i, j);
+        return sum;
     }
 
 public:
     NumArray(vector<int>& nums) {
-        if (nums.empty()) return;
+        this->nums = nums;
         n = nums.size();
-        tree.resize(4 * n); // Allocate memory for segment tree
-        buildTree(nums, 0, 0, n - 1);
+        fenwickTree.resize(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            updateFenwickTree(i + 1, nums[i]);
+        }
     }
 
-    void update(int index, int val) {
-        updateTree(0, 0, n - 1, index, val);
+    void update(int i, int val) {
+        int delta = val - nums[i];
+        nums[i] = val;
+        updateFenwickTree(i + 1, delta);
     }
 
     int sumRange(int i, int j) {
-        return sumRangeTree(0, 0, n - 1, i, j);
+        return getPrefixSum(j + 1) - getPrefixSum(i);
     }
 };
