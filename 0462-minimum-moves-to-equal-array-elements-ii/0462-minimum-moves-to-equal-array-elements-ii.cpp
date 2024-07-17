3242eb9470100583be6ab295232b5f098c4dce8e
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-17 12:00:19 +0530

    Time: 13 ms (23.73%), Space: 14.9 MB (5.42%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,7 +4,8 @@ public:
         int n = nums.size() , diff = 0;
         sort(nums.begin() , nums.end());
         int median = nums[nums.size()/2];
-        for(int i=0; i<nums.size(); i++) diff += abs(nums[i] - median);
+        for(int i=0; i<nums.size()/2; i++) //diff += abs(nums[i] - median);
+        diff += nums[n-1-i] - nums[i];
         return diff;
     }
 };
