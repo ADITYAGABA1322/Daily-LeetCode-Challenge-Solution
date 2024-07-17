f6b4dfbba165c66518f8cf1ec864e268e2868981
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-17 11:16:47 +0530

    Time: 49 ms (90.22%), Space: 41.1 MB (46.54%) - LeetHub

--- 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.cpp
@@ -8,17 +8,31 @@ public:
         return res;
     }
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long left = *min_element(nums.begin() ,nums.end());
-        long long right = *max_element(nums.begin() , nums.end());
-        long long ans = INT_MAX;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            long long cost1 = findCost(nums , cost , mid);
-            long long cost2 = findCost(nums , cost , mid+1);
-            ans = min(cost1 , cost2);
-            if(cost2 > cost1) right = mid -1;
-            else left = mid+1;
+        // long long left = *min_element(nums.begin() ,nums.end());
+        // long long right = *max_element(nums.begin() , nums.end());
+        // long long ans = INT_MAX;
+        // while(left <= right){
+        //     int mid = left + (right - left)/2;
+        //     long long cost1 = findCost(nums , cost , mid);
+        //     long long cost2 = findCost(nums , cost , mid+1);
+        //     ans = min(cost1 , cost2);
+        //     if(cost2 > cost1) right = mid -1;
+        //     else left = mid+1;
+        // }
+        // return ans == INT_MAX ? 0 : ans;
+        // 2nd one
+
+        long long l = 1 , r = 1000000 , cost1 = 0 , cost2 = 0;
+        while(l<r){
+            int m = l + (r-l)/2;
+            cost1 = cost2 = 0;
+            for(int i=0; i<nums.size(); i++){
+                cost1 += (long long) cost[i] * abs(nums[i] - m);
+                cost2 += (long long) cost[i] * abs(nums[i] - (m+1));
+            }
+            if(cost2 > cost1) r = m;
+            else l = m+1;
         }
-        return ans == INT_MAX ? 0 : ans;
+        return min(cost1 , cost2);
     }
 };
