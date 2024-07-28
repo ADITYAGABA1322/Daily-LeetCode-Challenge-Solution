fecfb5fe4814b8377e3d234ebde2d0835be88452
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-28 18:18:11 +0530

    Time: 7 ms (50%), Space: 27.9 MB (16.67%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.cpp
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.cpp
@@ -10,3 +10,26 @@ public:
         return sum1>sum2 || sum2>sum1;
     }
 };
+
+// or
+// #include <vector>
+// using namespace std;
+
+// class Solution {
+// public:
+//     bool canAliceWin(vector<int>& nums) {
+//         int sumSingleDigit = 0, sumDoubleDigit = 0;
+
+//         // Calculate the sums of single-digit and double-digit numbers
+//         for (int num : nums) {
+//             if (num >= 1 && num <= 9) {
+//                 sumSingleDigit += num;
+//             } else if (num >= 10 && num <= 99) {
+//                 sumDoubleDigit += num;
+//             }
+//         }
+
+//         // Alice wins if the sum of her chosen numbers is strictly greater than the sum of Bob's numbers
+//         return sumSingleDigit > sumDoubleDigit || sumDoubleDigit > sumSingleDigit;
+//     }
+// };
