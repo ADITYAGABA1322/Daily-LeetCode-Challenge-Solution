65b077aecec43e49edc16855a3b314f5b435be85
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 13:08:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n , 1);
+        dp[0] = 1;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(nums[i] > nums[j]) dp[i] = max(dp[i] , dp[j] + 1);
+            }
+        }
+        int maxi = 1;
+        for(auto val : dp) maxi = max(maxi , val);
+        return maxi;
+    }
+};
