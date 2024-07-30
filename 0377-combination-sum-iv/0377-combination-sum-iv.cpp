d2d5ffb38770c32c90c901720d01244fcc0478a8
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 13:26:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        sort(nums.begin() , nums.end());
+        vector<uint> dp(target +1); dp[0] = 1;
+        for(int i=1; i<=taregt; i++){
+            for(auto num : nums){
+                if(num <= i) dp[i] += dp[i-num];
+                else break;
+            }
+        }
+    }
+};
