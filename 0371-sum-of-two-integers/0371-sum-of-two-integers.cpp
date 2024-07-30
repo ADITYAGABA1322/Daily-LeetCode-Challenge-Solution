5066944dd88219cdd96898c38e4da33a5ee7107c
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 12:41:12 +0530

    Time: 0 ms (100%), Space: 7.4 MB (23.25%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        while(b != 0){
+        unsigned carry = (a&b)<<1;
+        a = a^b;
+        b = carry;
+        }
+        return a;
     }
 };
