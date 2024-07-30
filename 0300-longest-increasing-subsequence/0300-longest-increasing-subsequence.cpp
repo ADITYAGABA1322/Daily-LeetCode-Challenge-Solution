63d5d6d93279a3fc93618ccb65dc9b2221daf51c
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 13:08:49 +0530

    Time: 58 ms (77.43%), Space: 13.2 MB (47.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> dp(n , 1);
         dp[0] = 1;
         for(int i=1; i<n; i++){
-            for(int j=0; j<n; j++){
+            for(int j=0; j<i; j++){
                 if(nums[i] > nums[j]) dp[i] = max(dp[i] , dp[j] + 1);
             }
         }
