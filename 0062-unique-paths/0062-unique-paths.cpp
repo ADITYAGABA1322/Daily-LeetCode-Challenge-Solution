9cb68300164a39ace8d10fa8eeb2411df4336465
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 16:08:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n , int i=0 , int j = 0;) {
+    int uniquePaths(int m, int n , int i=0 , int j = 0) {
         if(i>=m && j>=n) return 0;
         if(i == m-1 && j == n-1) return 1;
         if(dp[i][j]) return dp[i][j];
