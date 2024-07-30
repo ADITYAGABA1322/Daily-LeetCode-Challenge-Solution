c653bddf66387e219573607efb744e994de8caf4
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 13:40:32 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n , 0);
-        dp[0] = nums[0];
-        for(int i=1; i<n; i++){
-            int take = nums[i];
-            if(i>1)
-                take  += dp[i-2];
-                int nottake = 0+dp[i-1];
-                dp[i] = max(take , nottake);
+        // vector<int> dp(n , 0);
+        // dp[0] = nums[0];
+        // for(int i=1; i<n; i++){
+        //     int take = nums[i];
+        //     if(i>1)
+        //         take  += dp[i-2];
+        //         int nottake = 0+dp[i-1];
+        //         dp[i] = max(take , nottake);
+        // }
+        // return dp[n-1];
+        int p1 = 0 , p2 = 0;
+        for(auto n : nums){
+            int tmp = p1;
+            p1 = max(p1 , n + p2);
+            p2 =tmp;
         }
-        return dp[n-1];
+        return p1;
     }
 };
 
