88bcc8f976fa2ca32dd9d87dc0b6f4d9e079a4a5
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-30 11:35:08 +0530

    Time: 28 ms (53.18%), Space: 38.3 MB (64.97%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n , 1);
+        int left = 1;
+        for(int i=0; i<n; i++){
+            res[i] *= left;
+            left *= nums[i];
+        }
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+};
