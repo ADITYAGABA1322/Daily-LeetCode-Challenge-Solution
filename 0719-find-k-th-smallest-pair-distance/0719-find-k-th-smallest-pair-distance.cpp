3de3b8353bdcdea253e742f00b80a4040bd4d8ee
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-08-15 01:43:04 +0530

    Time: 525 ms (8.34%), Space: 25.8 MB (5.07%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        int arraySize = nums.size();
+
+        // Find the maximum element in the array
+        int maxElement = *max_element(nums.begin(), nums.end());
+
+        // Initialize a bucket array to store counts of each distance
+        vector<int> distanceBucket(maxElement + 1, 0);
+
+        // Populate the bucket array with counts of each distance
+        for (int i = 0; i < arraySize; ++i) {
+            for (int j = i + 1; j < arraySize; ++j) {
+                // Compute the distance between nums[i] and nums[j]
+                int distance = abs(nums[i] - nums[j]);
+
+                // Increment the count for this distance in the bucket
+                ++distanceBucket[distance];
+            }
+        }
+
+        // Find the k-th smallest distance
+        for (int dist = 0; dist <= maxElement; ++dist) {
+            // Reduce k by the number of pairs with the current distance
+            k -= distanceBucket[dist];
+
+            // If k is less than or equal to 0, return the current distance
+            if (k <= 0) {
+                return dist;
+            }
+        }
+        return -1;  // Return -1 if no distance found, should not reach here
+    }
+};
