9daea8391a5419341231ebfb5d6a9a8844c8cf71
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-08-18 00:23:46 +0530

    Time: 215 ms (100%), Space: 189.2 MB (100%) - LeetHub

--- 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.cpp
+++ 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.cpp
@@ -1,27 +1,53 @@
+// class Solution {
+// public:
+//     vector<int> resultsArray(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> res;
+//         deque<int> dq;
+//         vector<int> dp(n , 1);
+//         for(int i=1; i<n; i++){
+//             if(nums[i] == nums[i-1] + 1) dp[i]=  dp[i-1] + 1;
+//         }
+//         for(int i=0; i<n; i++){
+//             if (!dq.empty() && dq.front() == i - k) {
+//                 dq.pop_front();
+//             }
+//             while(!dq.empty() && nums[dq.back()] < nums[i]){
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+//              if (i >= k - 1) {
+//                 if (dp[i] >= k) {
+//                     res.push_back(nums[dq.front()]);
+//                 } else {
+//                     res.push_back(-1);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        int n = nums.size();
         vector<int> res;
-        deque<int> dq;
+        int n = nums.size();
+        // for(int i=0 , cons = 1 ; i < nums.size(); i++){
+        //     if(i >0 && nums[i] == nums[i-1] + 1) cons++;
+        //     else
+        //         cons = 1;
+        //     if(i+1 >= k) res.push_back(cons >=k ? nums[i] : -1);
+        // }
+        // return res;
         vector<int> dp(n , 1);
-        for(int i=1; i<n; i++){
-            if(nums[i] == nums[i-1] + 1) dp[i]=  dp[i-1] + 1;
+        for(int i=1; i<n ;i++){
+            if(nums[i] == nums[i-1] + 1) dp[i] = dp[i-1]+1;
         }
         for(int i=0; i<n; i++){
-            if (!dq.empty() && dq.front() == i - k) {
-                dq.pop_front();
-            }
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-             if (i >= k - 1) {
-                if (dp[i] >= k) {
-                    res.push_back(nums[dq.front()]);
-                } else {
-                    res.push_back(-1);
-                }
+            if(i>=k-1){
+            if(dp[i] >= k) res.push_back(nums[i]);
+            else res.push_back(-1);
             }
         }
         return res;
