234f95bf4c19e41f0a8fe04439031d278922ef5e
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-10-03 23:02:13 +0530

    Time: 133 ms (64.27%), Space: 70.4 MB (73.78%) - LeetHub

---
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        int totalSum = 0;
+        for(auto num : nums){
+            totalSum = (totalSum + num) % p;
+        }
+        if(totalSum == 0) return 0;
+        unordered_map<int , int> mp;
+        mp[0] = -1;
+        int prefixSum = 0 , minLen = n;
+        for(int i=0; i<n; i++){
+            prefixSum = (prefixSum + nums[i]) % p;
+            int target = (prefixSum - totalSum + p) % p;
+            if(mp.find(target) != mp.end()){
+                minLen = min(minLen , i - mp[target]);
+            }
+            mp[prefixSum] = i;
+        }
+        return minLen == n ? -1 : minLen;
+    }
+};
