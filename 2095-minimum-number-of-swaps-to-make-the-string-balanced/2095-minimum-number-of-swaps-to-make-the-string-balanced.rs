4b6e5e6f00b034b92dae9b14987660ceeb92c72d
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-10-09 23:07:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.rs
@@ -0,0 +1,19 @@
+struct Solution;
+
+impl Solution {
+    pub fn min_swaps(s: String) -> i32 {
+        let mut stack_size = 0;
+        let n = s.len();
+
+        for ch in s.chars() {
+            if ch == '[' {
+                stack_size += 1;
+            } else {
+                if stack_size > 0 {
+                    stack_size -= 1;
+                }
+            }
+        }
+        (stack_size + 1) / 2
+    }
+}
