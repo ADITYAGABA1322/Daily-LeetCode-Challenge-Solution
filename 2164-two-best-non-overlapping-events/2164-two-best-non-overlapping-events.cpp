8566043343408b72cc7617a2bc138e3dd4330991
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-12-08 22:44:44 +0530

    Time: 110 ms (50.9%), Space: 139.4 MB (48.52%) - LeetHub

---
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        priority_queue<pair<int , int>, vector<pair<int , int>> , greater<pair<int , int>>> pq;
+        sort(events.begin() , events.end());
+        int maxVal = 0 , maxSum = 0;
+        for(auto e : events){
+            while(pq.size() && pq.top().first < e[0]){
+                maxVal = max(maxVal , pq.top().second);
+                pq.pop();
+            }
+            maxSum = max(maxSum , maxVal + e[2]);
+            pq.push({e[1] , e[2]});
+        } 
+        return maxSum;
+    }
+};
