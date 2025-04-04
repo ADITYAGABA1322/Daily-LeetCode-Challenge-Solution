ba167602a98aaf770401df89891a267bbea7c95a
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-04-04 22:31:44 +0530

    Time: 3 ms (19.3%), Space: 22 MB (67.37%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode*, int> dfs(TreeNode* root) {
+        if (!root) {
+            return {nullptr, 0};
+        }
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        if (left.second > right.second) {
+            return {left.first, left.second + 1};
+        }
+        if (left.second < right.second) {
+            return {right.first, right.second + 1};
+        }
+        return {root, left.second + 1};
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) { return dfs(root).first; }
+};
