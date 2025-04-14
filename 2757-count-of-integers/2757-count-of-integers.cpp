c276a31b49ef795b6f0322edc4d9dd2b78561f70
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-04-14 23:57:45 +0530

    Time: 59 ms (35.89%), Space: 49.7 MB (19.06%) - LeetHub

--- 2757-count-of-integers/2757-count-of-integers.cpp
+++ 2757-count-of-integers/2757-count-of-integers.cpp
@@ -2,46 +2,23 @@ class Solution {
 public:
     int count(string num1, string num2, int min_sum, int max_sum) {
         const int MOD = 1e9+7;
-          auto countUpTo = [&](const string& upper, int min_sum, int max_sum) -> int {
+        auto countUpTo = [&](const string& upper , int min_sum , int max_sum) -> int {
             int n = upper.length();
-            
-            // memo[pos][tight][sum] = count of valid numbers
-            vector<vector<vector<int>>> memo(n + 1, 
-                                            vector<vector<int>>(2, 
-                                                             vector<int>(401, -1)));
-            
-            function<int(int, bool, int)> dp = [&](int pos, bool tight, int sum) -> int {
-                // Base case: processed all digits
-                if (pos == n) {
-                    return (sum >= min_sum && sum <= max_sum) ? 1 : 0;
-                }
-                
-                // Return memoized result if available
-                if (memo[pos][tight][sum] != -1) return memo[pos][tight][sum];
-                
-                int result = 0;
-                // Maximum digit allowed at current position
+            vector<vector<vector<int>>> memo(n+1 , vector<vector<int>>(2 , vector<int>(401 , -1)));
+            function<int (int , bool , int)>dp = [&](int pos , bool tight , int sum) -> int {
+                if(pos == n) return (sum >= min_sum && sum<=max_sum) ? 1 : 0;
+                if(memo[pos][tight][sum] != -1) return memo[pos][tight][sum];
+                int res = 0;
                 int limit = tight ? (upper[pos] - '0') : 9;
-                
-                // Try each valid digit
-                for (int digit = 0; digit <= limit; digit++) {
-                    // Skip if sum would exceed max_sum
-                    if (sum + digit > max_sum) continue;
-                    
-                    // Next position's tight constraint
+                for(int digit =0; digit<=limit; digit++){
+                    if(sum+digit > max_sum) continue;
                     bool nextTight = tight && (digit == upper[pos] - '0');
-                    
-                    // Add count from recursive call
-                    result = (result + dp(pos + 1, nextTight, sum + digit)) % MOD;
-                }
-                
-                // Memoize and return
-                return memo[pos][tight][sum] = result;
+                    res = (res + dp(pos+1 , nextTight , sum + digit)) % MOD;
+                } 
+                return memo[pos][tight][sum] = res;
             };
-            
-            return dp(0, true, 0);
-        };
-        
+            return dp(0 , true , 0);
+       };
        int low_sum = 0;
        for(auto c : num1) low_sum += c -'0';
        bool includenum1 = (low_sum >= min_sum && low_sum <= max_sum) ? true : false;
@@ -51,16 +28,20 @@ public:
        return res;
     }
     private:
-    string decrementString(const string& num){
+    string decrementString(string &num){
         string res = num;
         int i = res.length()-1;
         while(i>=0 && res[i] == '0'){
             res[i] = '9';
             i--;
         }
-        if(i>=0) res[i]--;
-        else return "0";
-        if(res[i] == '0' && res.length() > 1) return res.substr(1);
+        if(i >= 0){
+            res[i]--;
+        }
+        else{
+            return "0";
+        }
+        if(res[0] == '0' && res.length() > 1) return res.substr(1);
         return res;
     }
 };
