1660c6cd63e07e08763f1f1392fe9670905c9e37
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-04-17 19:06:12 +0530

    Time: 0 ms (100%), Space: 15.4 MB (91.08%) - LeetHub

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int res = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] == nums[j] && ((i*j) % k  == 0)) res++;
+            }
+        }
+        return res;
+    }
+};
