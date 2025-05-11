472bee007645898a707e2440074f59a5ec37c66c
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-05-11 22:35:43 +0530

    Time: 0 ms (100%), Space: 11.9 MB (20.63%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int n = arr.size();
-        // Check if there are at least 3 elements in the array
-        if(n < 3) return false;
-        
-        for(int i = 0; i < n - 2; i++) {
-            // Check if the current, next, and next to next elements are odd
-            if(arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0) {
-                return true;
-            }
+        if(arr.size() < 3) return false;
+        for(int i=0; i<arr.size() - 2; i++){
+            if(arr[i] % 2 != 0 && arr[i+1]% 2 != 0 && arr[i+2] % 2 != 0) return true;
         }
-        // If no three consecutive odds are found, return false
         return false;
     }
 };
