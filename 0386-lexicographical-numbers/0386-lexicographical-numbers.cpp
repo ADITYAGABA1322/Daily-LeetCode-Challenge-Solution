49db9b4b508f7053d3548372777c757a033d2413
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-06-08 19:09:43 +0530

    Time: 0 ms (100%), Space: 14 MB (69.54%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,7 +1,3 @@
-// brute force -> approach
-// dekhke khyal ki range hi sb kuch and lexo nikal lenge lekin range ko dekhke 
-// main toh fomrula lexograhoical pta ha toh sab asan ha
-//  cases for 10 th, 1-
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
@@ -9,13 +5,11 @@ public:
         vector<int> res;
         for(int i=0; i<n; i++){
             res.push_back(curr);
-            if(curr*10 <= n) curr *= 10;
-            else{
-                if(curr>=n) curr/=10;
-                 curr++;
-                while(curr % 10 == 0){
-                    curr /= 10;
-                }
+            if(curr * 10 <= n ) curr *= 10;
+            else {
+                if(curr >=n) curr /= 10;
+                curr++;
+                while(curr%10 == 0) curr /= 10;
             }
         }
         return res;
