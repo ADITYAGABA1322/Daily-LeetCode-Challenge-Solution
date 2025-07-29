c48388ca709538eabd387a3edf7709ae8846b191
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-07-30 00:38:01 +0530

    Time: 26 ms (65.95%), Space: 60.7 MB (54.74%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -2,30 +2,18 @@ class Solution {
 public:
     vector<int> smallestSubarrays(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n);
-        vector<int> lastPos(30, -1); // Track last position of each bit (0-29)
+        vector<int> res(n) , lastPos(30 , -1);
+        for(int i=n-1; i>=0; i--){
+            for(int bit = 0; bit < 30; bit++){
+                if((nums[i] >> bit) & 1) lastPos[bit] = i;
+            }
         
-        // Process from right to left
-        for (int i = n - 1; i >= 0; i--) {
-            // Update last positions for bits in current number
-            for (int bit = 0; bit < 30; bit++) {
-                if ((nums[i] >> bit) & 1) {
-                    lastPos[bit] = i;
-                }
-            }
-            
-            // Find the rightmost position we need to reach
-            // to get maximum possible OR starting from i
-            int maxReach = i; // At least include current element
-            for (int bit = 0; bit < 30; bit++) {
-                if (lastPos[bit] != -1) {
-                    maxReach = max(maxReach, lastPos[bit]);
-                }
-            }
-            
-            result[i] = maxReach - i + 1;
+        int maxReach = i;
+        for(int bit =0; bit < 30 ; bit++){
+            if(lastPos[bit] != -1) maxReach = max(maxReach , lastPos[bit]);
         }
-        
-        return result;
+        res[i] = maxReach - i + 1;
+        }
+        return res;
     }
 };
