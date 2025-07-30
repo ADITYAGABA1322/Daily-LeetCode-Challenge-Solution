d00db53c01f08f3b1fa626de7d02d39920d1148e
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-07-31 00:43:13 +0530

    Time: 7 ms (10.04%), Space: 86.2 MB (38.1%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,27 +1,15 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int longestSubarray(std::vector<int>& arr) {
-        if (arr.empty()) return 0; // Handle empty array case
-
-        // Find the maximum element
-        int maxElement = *std::max_element(arr.begin(), arr.end());
-
-        // Find the longest contiguous subarray of the maximum element
-        int maxLength = 0;
-        int currentLength = 0;
-
-        for (int num : arr) {
-            if (num == maxElement) {
-                currentLength++;
-                maxLength = std::max(maxLength, currentLength);
-            } else {
-                currentLength = 0;
+    int longestSubarray(vector<int>& nums) {
+        int maxEle = *max_element(nums.begin() , nums.end());
+        int max_len = 0 , curr_len = 0;
+        for(auto num : nums){
+            if(num == maxEle){
+                curr_len++;
+                max_len = max(max_len , curr_len);
             }
+            else curr_len = 0;
         }
-
-        return maxLength;
+        return max_len;
     }
 };
