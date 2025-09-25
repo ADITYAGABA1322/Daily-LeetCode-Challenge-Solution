ed5188cef3802fc600b0cc046a549972bfdabe78
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-09-26 00:34:52 +0530

    Time: 0 ms (100%), Space: 12.8 MB (66.61%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp = triangle[n-1];
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                dp[j] = min(dp[j] , dp[j+1]) + triangle[i][j];
+            }
+        }
+        return dp[0];
+    }
+};
