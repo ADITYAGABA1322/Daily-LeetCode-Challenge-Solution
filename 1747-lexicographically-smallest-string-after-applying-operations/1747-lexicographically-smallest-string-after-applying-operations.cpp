36f9abee5ebcb4d6261567933b41885cf707d31a
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-10-20 01:55:28 +0530

    Time: 55 ms (77.34%), Space: 25.9 MB (75.39%) - LeetHub

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string findLexSmallestString(string s, int a, int b) {
+        int n = s.size();
+        vector<int> vis(n);
+        string res = s;
+        // double the length of s for convenience in extracting the rotated
+        // string t
+        s = s + s;
+        for (int i = 0; vis[i] == 0; i = (i + b) % n) {
+            vis[i] = 1;
+            for (int j = 0; j < 10; j++) {
+                int k_limit = b % 2 == 0 ? 0 : 9;
+                for (int k = 0; k <= k_limit; k++) {
+                    // before each accumulation, re-truncate t
+                    string t = s.substr(i, n);
+                    for (int p = 1; p < n; p += 2) {
+                        t[p] = '0' + (t[p] - '0' + j * a) % 10;
+                    }
+                    for (int p = 0; p < n; p += 2) {
+                        t[p] = '0' + (t[p] - '0' + k * a) % 10;
+                    }
+                    res = min(res, t);
+                }
+            }
+        }
+        return res;
+    }
+};
