d81dbd89e7918532a6fb91bc450a5a7ba5644332
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-12-04 00:50:08 +0530

    Time: 1181 ms (87.57%), Space: 309.6 MB (94.08%) - LeetHub

---
+++ 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        int n = points.size();
+        int inf = 1e9 + 7;
+        unordered_map<float, vector<float>> slopeToIntercept;
+        unordered_map<int, vector<float>> midToSlope;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                float k, b;
+                if (x2 == x1) {
+                    k = inf;
+                    b = x1;
+                } else {
+                    k = (float)(y2 - y1) / (x2 - x1);
+                    b = (float)(y1 * dx - x1 * dy) / dx;
+                }
+                int mid = (x1 + x2) * 10000 + (y1 + y2);
+                slopeToIntercept[k].push_back(b);
+                midToSlope[mid].push_back(k);
+            }
+        }
+        for (auto& [_, sti] : slopeToIntercept) {
+            if (sti.size() == 1) {
+                continue;
+            }
+            map<float, int> cnt;
+            for (float b : sti) {
+                cnt[b]++;
+            }
+            int sum = 0;
+            for (auto& [_, count] : cnt) {
+                ans += sum * count;
+                sum += count;
+            }
+        }
+        for (auto& [_, mts] : midToSlope) {
+            if (mts.size() == 1) {
+                continue;
+            }
+            map<float, int> cnt;
+            for (float k : mts) {
+                cnt[k]++;
+            }
+            int sum = 0;
+            for (auto& [_, count] : cnt) {
+                ans -= sum * count;
+                sum += count;
+            }
+        }
+        return ans;
+    }
+};
