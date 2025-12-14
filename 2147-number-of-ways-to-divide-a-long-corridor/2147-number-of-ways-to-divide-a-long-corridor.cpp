e59a778bb9fd988690b56f450b1b70a42591a0e4
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2025-12-14 23:47:52 +0530

    Time: 9 ms (96.5%), Space: 33.6 MB (81.5%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        // Store 1000000007 in a variable for convenience
+        const int MOD = 1e9 + 7;
+
+        // Initial values of three variables
+        int zero = 0;
+        int one = 0;
+        int two = 1;
+
+        // Compute using derived equations
+        for (char thing : corridor) {
+            if (thing == 'S') {
+                zero = one;
+                swap(one, two);
+            } else {
+                two = (two + zero) % MOD;
+            }
+        }
+
+        // Return the result
+        return zero;
+    }
+};
