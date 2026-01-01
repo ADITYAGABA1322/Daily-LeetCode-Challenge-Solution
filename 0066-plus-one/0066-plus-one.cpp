bfc37904f9b79a892e9a7032e8448af130654d75
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-01-01 23:19:29 +0530

    Time: 0 ms (100%), Space: 11.5 MB (77.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;  i>=0; i--){
+            if(digits[i] == 9) digits[i] = 0;
+            else { digits[i] += 1; return digits;}
+        }
+        digits.push_back(0);
+        digits[0] = 1;
+        return digits;
+    }
+};
