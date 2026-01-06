f84548cfe3a96f94f26f8d1bd7406507e9d15452
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-01-06 23:48:44 +0530

    Time: 8 ms (24.97%), Space: 109.6 MB (13.47%) - LeetHub

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */  
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int maxSum = INT_MIN;
+        int ans = 0, level = 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            level++;
+            int sumAtCurrentLevel = 0;
+            // Iterate over all the nodes in the current level.
+            for (int sz = q.size(); sz > 0; --sz) {
+                TreeNode* node = q.front();
+                q.pop();
+                sumAtCurrentLevel += node->val;
+
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+
+            if (maxSum < sumAtCurrentLevel) {
+                maxSum = sumAtCurrentLevel;
+                ans = level;
+            }
+        }
+
+        return ans;
+    }
+};
