4f728fc58a57a73ee18605563f46f35f03e59af9
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-01-29 23:09:46 +0530

    Time: 16 ms (94.92%), Space: 95.4 MB (96.54%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        long long res = 0;
+        int dp[26][26];
+        for(int i=0; i<26; i++){
+            for(int j=0; j<26; j++){
+                dp[i][j] = 1e9+10;
+            }
+            dp[i][i] = 0;
+        }
+        for(int i=0; i<cost.size(); i++){
+            dp[original[i] - 'a'][changed[i] - 'a'] = min(dp[original[i] - 'a'][changed[i] - 'a'] , cost[i]);
+        }
+
+        for(int k=0; k<26; k++){
+            for(int i=0; i<26; i++){
+                for(int j=0; j<26; j++){
+                    dp[i][j] = min(dp[i][j] , dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+
+        for(int i=0; i<source.size(); i++){
+            if(source[i] == target[i]) continue;
+            int s = source[i] - 'a' , t = target[i]-'a';
+            if(dp[s][t] > 1e9) return -1;
+            res += dp[s][t];
+        }
+        return res;
+    }
+};
