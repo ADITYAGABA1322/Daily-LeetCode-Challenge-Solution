a26cf7ed56477410158de1b0aaa84201f35da7f0
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-02-17 23:02:01 +0530

    Time: 0 ms (100%), Space: 9.7 MB (41.32%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> res;
+        for(int i=0; i<12; i++){
+            for(int j=0; j<60; j++){
+                if(__builtin_popcount(i) + __builtin_popcount(j) ==  turnedOn){
+                    res.push_back(to_string(i) + ":" + (j<10 ? "0" : "")+ to_string(j));
+                }
+            }
+        }
+        return res;
+    }
+};
