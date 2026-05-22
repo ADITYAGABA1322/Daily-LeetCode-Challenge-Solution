9e74ad127c5916e2121f8ead0a8cefee1b1e2c4c
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-05-22 23:44:24 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int low = 0 , high = n-1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] >= nums[low]){
-                if(target >= nums[low] && target < nums[mid]) high = mid-1;
-                else low = mid + 1;
+        int l = 0 , r = n-1;
+        while(l<=r){
+            int m = l + (r-l) / 2;
+            if(nums[m] == target) return m;
+            else if(nums[m] >= nums[l]){
+                if(target >= nums[l] && target < nums[m]) r = m-1;
+                else l = m +1;
             }
             else{
-                if(target <= nums[high] && target > nums[mid]) low = mid+1;
-                else high = mid-1;
+                if(target<=nums[r] && target > nums[m]) l = m +1;
+                else r = m - 1;
             }
         }
         return -1;
