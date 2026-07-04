fcda0c04bee1d4004c6a8636422e401a1b13839b
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-07-05 00:26:37 +0530

    Time: 131 ms (13.56%), Space: 151.4 MB (8.98%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,30 +1,56 @@
+// bfs
+// class Solution {
+// public:
+//     int bfs(int n , vector<vector<pair<int , int>>>& g){
+//         vector<bool> vis(n+1);
+//         queue<int> q;
+//         int ans = INT_MAX;
+//         q.push(1);
+//         vis[1] = true;
+//         while(!q.empty()){
+//             auto node = q.front();
+//             q.pop();
+//             for(auto child : g[node]){
+//                 ans = min(ans , child.second);
+//                 if(!vis[child.first]){
+//                     vis[child.first] = true;
+//                     q.push(child.first);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+//     int minScore(int n, vector<vector<int>>& roads) {
+//         vector<vector<pair<int , int>>> g(n+1);
+//         for(auto r : roads){
+//             g[r[0]].push_back({r[1] , r[2]});
+//             g[r[1]].push_back({r[0] , r[2]});
+//         }
+//         return bfs(n , g);
+//     }
+// };
+
+// dfs
 class Solution {
 public:
-    int bfs(int n , vector<vector<pair<int , int>>>& g){
-        vector<bool> vis(n+1);
-        queue<int> q;
-        int ans = INT_MAX;
-        q.push(1);
-        vis[1] = true;
-        while(!q.empty()){
-            auto node = q.front();
-            q.pop();
-            for(auto child : g[node]){
-                ans = min(ans , child.second);
-                if(!vis[child.first]){
-                    vis[child.first] = true;
-                    q.push(child.first);
-                }
-            }
+    void dfs(int u , vector<bool>& vis , unordered_map<int , vector<pair<int , int>>>& adj ,  int& res){
+        vis[u] = true;
+        for(auto p: adj[u]){
+            int v = p.first , d = p.second;
+            res = min(res , d);
+            if(!vis[v]) dfs(v , vis , adj , res);
         }
-        return ans;
     }
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int , int>>> g(n+1);
-        for(auto r : roads){
-            g[r[0]].push_back({r[1] , r[2]});
-            g[r[1]].push_back({r[0] , r[2]});
+        unordered_map<int , vector<pair<int , int>>> adj;
+        for(auto r:  roads){
+            int u = r[0] , v = r[1] , d = r[2];
+            adj[u].push_back({v , d});
+            adj[v].push_back({u , d});
         }
-        return bfs(n , g);
+        vector<bool> vis(n , false);
+        int res = INT_MAX;
+        dfs(1 , vis , adj , res);
+        return res;
     }
 };
