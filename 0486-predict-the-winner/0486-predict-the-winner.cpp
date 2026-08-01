19820b4f05b71d91a0a7823517d6166550e99447
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-08-02 00:34:02 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-
-  // bool predictTheWinner(vector<int>& nums) {
+    // bool predictTheWinner(vector<int>& nums) {
     //     int n = nums.size();
     //     int total = accumulate(nums.begin() , nums.end() , 0);
     //     int player1_score = solve(0 , n-1 , nums);
@@ -13,26 +12,51 @@ public:
     //     if(i > j) return 0;
     //     if(i == j) return nums[i];
     //     // Max - Min Game Strategy
-    //     int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1 , j-1 , nums));
-    //     int take_j = nums[j] + min(solve(i , j-2, nums) , solve(i+1 , j-1 , nums));
-    //     return max(take_i , take_j);
+    //     int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1 , j-1 ,
+    //     nums)); int take_j = nums[j] + min(solve(i , j-2, nums) , solve(i+1 ,
+    //     j-1 , nums)); return max(take_i , take_j);
     // }
 
     // memo + recursion II
-    int dp[23][23];
+    // int dp[23][23];
+    // bool predictTheWinner(vector<int>& nums) {
+    //     int n = nums.size();
+    //     memset(dp , -1 , sizeof(dp));
+    //     return solve(nums , 0 , n-1) >= 0;
+    // }
+
+    // int solve(vector<int>& nums , int i , int j){
+    //     if(i>j) return 0;
+    //     if(i == j) return nums[i];
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int take_i = nums[i] - solve(nums , i+1 , j);
+    //     int take_j = nums[j] - solve(nums , i , j-1);
+    //     return dp[i][j] = max(take_i , take_j);
+    // }
+
+    // bottom up do
+    // bool predictTheWinner(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+
+    //     for(int i=n-1; i>=0; i--){
+    //         dp[i][i] = nums[i];
+    //         for(int j=i+1; j<n; j++){
+    //             dp[i][j] = max(nums[i] - dp[i+1][j] , nums[j] - dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[0][n-1] >= 0;
+    // }
+    // bottom up space optimized
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        memset(dp , -1 , sizeof(dp));
-        return solve(nums , 0 , n-1) >= 0;
+        vector<int> dp(n);
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[n - 1] >= 0;
     }
-
-    int solve(vector<int>& nums , int i , int j){
-        if(i>j) return 0;
-        if(i == j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        int take_i = nums[i] - solve(nums , i+1 , j);
-        int take_j = nums[j] - solve(nums , i , j-1);
-        return dp[i][j] = max(take_i , take_j);
-    }
-    
 };
