6c4c5df216a543984eb35748f776fdc49ed90ba9
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-08-02 00:34:21 +0530

    Time: 0 ms (100%), Space: 9.7 MB (56.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -51,7 +51,7 @@ public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n);
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             dp[i] = nums[i];
             for (int j = i + 1; j < n; j++) {
                 dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
