4f6bfd74b99e9af591a6d017508c845b26ea1c8e
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-08-06 23:53:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (73.4%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        auto check = [&](int num) -> bool {
+            int product = 1;
+            while(num > 0){
+                product *= (num % 10);
+                num /= 10;
+                if(!product){
+                    break;
+                }
+            }
+            return !(product % t);
+        };
+        while(!check(n)){
+            n++;
+        }
+        return n;
+    }
+};
