e8157f91685ce038124fd23386653f622a6d1525
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-08-15 00:40:52 +0530

    Time: 6 ms (18.97%), Space: 9.7 MB (44.69%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         int n = s.size();
-        int res = 0;
-        for(int i=0; i<n; i++){
-            unordered_map<char , int> mp;
-            for(int j=i; j<n; j++){
-                mp[s[j]]++;
-                if(mp[s[j]] > 2) break;
-                res = max(res , j-i+1);
+        int res = 0 , i = 0;
+        unordered_map<char , int> mp;
+        // for(int i=0; i<n; i++){
+        //     unordered_map<char , int> mp;
+        //     for(int j=i; j<n; j++){
+        //         mp[s[j]]++;
+        //         if(mp[s[j]] > 2) break;
+        //         res = max(res , j-i+1);
+        //     }
+        // }
+        for(int j=0; j<n; j++){
+            mp[s[j]]++;
+            while(mp[s[j]] > 2){
+                mp[s[i]]--;
+                i++;
             }
+            res = max(res , j-i+1);
         }
         return res;
     }
