7bfe6fb21f5438d50933ed7200c967fcd3392213
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2026-08-19 01:35:57 +0530

    Time: 0 ms (100%), Space: 29 MB (80.36%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == k) return *max_element(nums.begin() , nums.end());
+        int count[51] = {};
+        for(auto x: nums) count[x]++;
+        if(k == 1){
+            for(int i=50; i>=0; i--){
+                if(count[i] == 1) return i;
+            }
+            return -1;
+        } 
+        int res = -1;
+        if(count[nums[0]] == 1) res = max(res , nums[0]);
+        if(count[nums[n-1]] == 1) res = max(res , nums[n-1]);
+        return res;
+    }
+};
